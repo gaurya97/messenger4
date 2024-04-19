e44938b4e97bcb63c6279e9bac1fec35765d4d67
gaurya97 <119463762+gaurya97@users.noreply.github.com>
2024-04-19 09:40:52 +0530

    Update dbconfig.js

--- routes/dbconfig.js
+++ routes/dbconfig.js
@@ -1,7 +1,7 @@
 const mysql=require('mysql2');
 module.exports=mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'gaurav1999',
-    database: 'test'
+    host: 'bo5v97j8ykdj4kjga5as-mysql.services.clever-cloud.com',
+    user: 'udl9pw4q7rqy46rb',
+    password: 'C0GxhgAOy1zXuuDuXWV6',
+    database: 'b685ynbstfctdjsha3pr'
 })
